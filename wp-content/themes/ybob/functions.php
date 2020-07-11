57bfe49eb8acf848a3170dd7255887ccd5d1a1b9
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-07-11 18:27:48 -0400

    added organization cpt #8

--- wp-content/themes/ybob/functions.php
+++ wp-content/themes/ybob/functions.php
@@ -154,6 +154,71 @@ function ybob_site_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'ybob_site_scripts' );
 
+
+/**
+ * Custom Post Types
+ * 
+ * 
+ */
+// Register Custom Post Type
+function ybob_org() {
+
+	$labels = array(
+		'name'                  => _x( 'Organizations', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Organization', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Organizations', 'text_domain' ),
+		'name_admin_bar'        => __( 'Organization', 'text_domain' ),
+		'archives'              => __( 'Organization Archives', 'text_domain' ),
+		'attributes'            => __( 'Organization Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Organization:', 'text_domain' ),
+		'all_items'             => __( 'All Organizations', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Organization', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Organization', 'text_domain' ),
+		'edit_item'             => __( 'Edit Organization', 'text_domain' ),
+		'update_item'           => __( 'Update Organization', 'text_domain' ),
+		'view_item'             => __( 'View Organization', 'text_domain' ),
+		'view_items'            => __( 'View Organizations', 'text_domain' ),
+		'search_items'          => __( 'Search Organization', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this organization', 'text_domain' ),
+		'items_list'            => __( 'Organizations list', 'text_domain' ),
+		'items_list_navigation' => __( 'Organizations list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter organizations list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Organization', 'text_domain' ),
+		'description'           => __( 'Organization / Business post type', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail', 'trackbacks', 'revisions', 'custom-fields', 'page-attributes', 'post-formats' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+		'show_in_rest'          => true,
+	);
+	register_post_type( 'ybob_org', $args );
+
+}
+add_action( 'init', 'ybob_org', 0 );
+
+
+
 /**
  * Implement the Custom Header feature.
  */
