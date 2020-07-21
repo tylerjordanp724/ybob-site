a95475fe982fcda5fd63d33228fdb2309f40bf6c
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-07-21 00:18:26 -0400

    primary hero styles

--- wp-content/themes/ybob/components/hero/hero.php
+++ wp-content/themes/ybob/components/hero/hero.php
@@ -9,11 +9,13 @@
 
 <div class="hero">
     <div class="hero-wrapper align-vert-center row-y-inside--sm">
-        <div class="hero-inner">
-            <span>phl</span>
+        <div class="hero-inner hero-inner--center text-align--center text--white">
+            <span class="f-label">phl</span>
             <h1>Black Owned</h1>
-            <span>businesses</span>
-            <div class="hero-text"></div>
+            <span class="f-label">businesses</span>
+            <div class="hero-text">
+                <p>this is some hero text.</p>
+            </div>
             <button class="btn">submit a business</button>
         </div>
     </div>
