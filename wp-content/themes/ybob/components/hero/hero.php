b98b403bc29a8bc1a8374b23680035e27a4934e2
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-07-21 00:39:35 -0400

    wip hero styles

--- wp-content/themes/ybob/components/hero/hero.php
+++ wp-content/themes/ybob/components/hero/hero.php
@@ -10,9 +10,11 @@
 <div class="hero">
     <div class="hero-wrapper align-vert-center row-y-inside--sm">
         <div class="hero-inner hero-inner--center text-align--center text--white">
-            <span class="f-label">phl</span>
-            <h1>Black Owned</h1>
-            <span class="f-label">businesses</span>
+            <div class="hero-title-row row-btm-outside--sm">
+                <span class="f-label">phl</span>
+                <h1 class="f-title">Black Owned</h1>
+                <span class="f-label">businesses</span>
+            </div>
             <div class="hero-text">
                 <p>this is some hero text.</p>
             </div>
