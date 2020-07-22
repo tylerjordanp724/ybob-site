7368ba25403b82ab93aac307e235d7d5c692a7d9
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-07-22 19:58:10 -0400

    querying for orgs #16

--- wp-content/themes/ybob/front-page.php
+++ wp-content/themes/ybob/front-page.php
@@ -9,5 +9,19 @@ get_header();
 <?php get_template_part('components/hero/hero');?>
 <div class="main-content">
 <!-- orgs go here -->
+    
+    <?php 
+        $args = array('post_type' => 'ybob_org', 'posts_per_page' => -1);
+        $orgs = new WP_Query($args);
+    ?>
+    <?php if($orgs->have_posts()):?>
+        <div class="orgs">
+            <?php while($orgs->have_posts()): $orgs->the_post();?>
+                something
+            <?php endwhile; wp_reset_postdata();?>
+        </div>
+    <?php endif;?>
+
+   
 </div>
 <?php get_footer();?>
