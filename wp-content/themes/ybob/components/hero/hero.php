14cb44a9bc5c818b3a376d0f5f61ac6f50b134f9
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-08-01 01:50:18 -0400

    wrapping up submission styles and moved fonts to css folder

--- wp-content/themes/ybob/components/hero/hero.php
+++ wp-content/themes/ybob/components/hero/hero.php
@@ -10,7 +10,7 @@ $header_text = get_field('header_text', 'option');
 ?>
 
 <div class="hero">
-    <div class="hero-wrapper align-vert-center row-inside--lg">
+    <div class="hero-wrapper align-vert-center row-inside--md">
         <div class="hero-inner hero-inner--center container--sm text-align--center text--blue-3">
             <div class="hero-title-row row-btm-outside--sm">
                 <span class="f-label">phl</span>
@@ -22,7 +22,7 @@ $header_text = get_field('header_text', 'option');
                     <?php echo $header_text;?>
                 </div>
             <?php endif;?>
-            <a href="<?php if($curr_slug === ''){echo get_site_url().'/submissions';}elseif($curr_slug === 'submissions'){echo get_option('home');}?>" class="btn btn--center"><?php if($curr_slug === ''){echo 'submit a business';}elseif($curr_slug === 'submissions'){echo '<span class="back-arrow"></span>'.'back to home';}?></a>
+            <a href="<?php if($curr_slug === ''){echo get_site_url().'/submissions';}elseif($curr_slug === 'submissions'){echo get_option('home');}?>" class="btn btn--center <?php if($curr_slug === 'submissions'){ echo 'btn--back-btn';}?>"><?php if($curr_slug === ''){echo 'submit a business';}elseif($curr_slug === 'submissions'){echo '<span class="back-arrow"></span>'.'back';}?></a>
         </div>
     </div>
 </div>
--- wp-content/themes/ybob/footer.php
+++ wp-content/themes/ybob/footer.php
@@ -11,10 +11,11 @@
 
 ?>
 
-	<footer class="site-footer">
-		<div class="site-info">
-			
-			
+	<footer class="site-footer row-inside--sm">
+		<div class="site-info container--lg grid-flex grid-flex--md justify--between align--center text--blue-3 f-body f-body--sm">
+			<div class="col col-md-4 row-btm-outside--sm text-align--center text-align--left-md">&copy;<?php echo date('Y');?></div>
+			<div class="col col-md-4 row-btm-outside--sm text-align--center">Donate</div>
+			<div class="col col-md-4 row-btm-outside--sm text-align--center text-align--right-md">Created by <a href="">@crystalfolkes</a> <a href="">@iamtylerjordan</a> <a href="">@authentic_spic3</a></div>
 			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
