094c67a84f84bc391399447880e2d9e18386261d
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-08-01 20:40:44 -0400

    editing footer content and adding input bg svgs

--- wp-content/themes/ybob/footer.php
+++ wp-content/themes/ybob/footer.php
@@ -13,9 +13,9 @@
 
 	<footer class="site-footer row-inside--sm">
 		<div class="site-info container--lg grid-flex grid-flex--md justify--between align--center text--blue-3 f-body f-body--sm">
-			<div class="col col-md-4 row-btm-outside--sm text-align--center text-align--left-md">&copy;<?php echo date('Y');?></div>
-			<div class="col col-md-4 row-btm-outside--sm text-align--center">Donate</div>
-			<div class="col col-md-4 row-btm-outside--sm text-align--center text-align--right-md">Created by <a href="">@crystalfolkes</a> <a href="">@iamtylerjordan</a> <a href="">@authentic_spic3</a></div>
+			<div class="col col-md-4 row-btm-outside--sm text-align--center text-align--left-md">&copy; Uplift&nbsp;<?php echo date('Y');?></div>
+			<!-- <div class="col col-md-4 row-btm-outside--sm text-align--center">Donate</div> -->
+			<div class="col col-md-4 row-btm-outside--sm text-align--center text-align--right-md">Created by &nbsp;<a href="https://www.instagram.com/crystalfolkes/" target="_blank">@crystalfolkes</a> &nbsp;<a href="https://www.instagram.com/iamtylerjordan/" target="_blank">@iamtylerjordan</a> &nbsp;<a href="https://www.instagram.com/authentic_spic3/" target="_blank">@authentic_spic3</a></div>
 			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
