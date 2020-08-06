ccaad9b4f82cbc7e7edef4053545d902d2729948
Tyler J. Perry <iamtylerjordan@gmail.com>
2020-08-05 21:27:10 -0400

    added favicon code

--- wp-content/themes/ybob/header.php
+++ wp-content/themes/ybob/header.php
@@ -17,6 +17,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Uplift The Block is a Philadelphia-based, user-friendly directory list for Black-owned businesses and entrepreneurs near you.">
 	<meta property="og:image" content="<?php bloginfo('template_url'); ?>/fb-share.jpg" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php bloginfo('template_url'); ?>/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php bloginfo('template_url'); ?>/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php bloginfo('template_url'); ?>/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php bloginfo('template_url'); ?>/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php bloginfo('template_url'); ?>/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php bloginfo('template_url'); ?>/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php bloginfo('template_url'); ?>/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php bloginfo('template_url'); ?>/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_url'); ?>/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="<?php bloginfo('template_url'); ?>/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php bloginfo('template_url'); ?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?php bloginfo('template_url'); ?>/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php bloginfo('template_url'); ?>/favicon-16x16.png">
+	<link rel="manifest" href="<?php bloginfo('template_url'); ?>/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="<?php bloginfo('template_url'); ?>/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
